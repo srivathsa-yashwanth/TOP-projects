ec049e6ce0d23739e7262df4c3b322153467b7fd
yash <yashwanthvs@gmail.com>
2022-02-08 04:26:35 +0530

    fully functional with proper css

--- sketch_pad/script.js
+++ sketch_pad/script.js
@@ -1,6 +1,91 @@
+
+
+function random(n)
+{
+    return Math.floor(Math.random()*n);
+}
+
+function addColor(event)
+{
+    event.target.style.backgroundColor = getColor(boolini);
+    
+}
+
+function started(event)
+{
+    event.target.style.backgroundColor = getColor(boolini);
+    cells.forEach((cell)=>cell.addEventListener('mouseenter',addColor));
+    
+}
+
+function stopped(event)
+{
+    cells.forEach((cell) => cell.removeEventListener('mouseenter',addColor));
+}
+
+function clearAll(event)
+{
+    cells.forEach((cell) => cell.style.backgroundColor = 'white')
+}
+
+
+
+function setSize(event)
+{
+    side = event.target.value;  
+
+    while (grid.firstChild)
+    {
+        grid.removeChild(grid.firstChild);
+    }
+    
+    for (let i=0; i<side*side; i++)
+    {
+        newDiv = document.createElement('div');
+        newDiv.classList.add("cell");
+        newDiv.setAttribute('draggable','false');
+        newDiv.style.cssText = `height: ${700/side}px; width:${700/side}px`;
+
+        const r1 = random(256);
+        const r2 = random(256);
+        const r3 = random(256);
+        //newDiv.style.backgroundColor = `rgb(${r1},${r2},${r3})`;
+        
+        grid.appendChild(newDiv);
+    }
+
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => cell.addEventListener('mousedown',started));
+    cells.forEach((cell)=>cell.addEventListener('mouseup',stopped));
+
+    cells.forEach((cell)=> cell.addEventListener('dragstart', (e) => { e.preventDefault() }))
+    cells.forEach((cell)=> cell.addEventListener('drop', (e) => { e.preventDefault() }))
+
+}
+
+function getColor(flag)
+{
+    if (flag)
+    {
+        const r1 = random(256);
+        const r2 = random(256);
+        const r3 = random(256);
+        return `rgb(${r1},${r2},${r3})`;
+    }
+    else
+    {
+        return 'black';    
+    }
+}
+
+let side;
+let newDiv;
+let boolini = false;
+
+
+
 grid = document.querySelector('#grids');
 
-let newDiv;
 for (let i=0; i<32*32; i++)
 {
     newDiv = document.createElement('div');
@@ -15,28 +100,26 @@ for (let i=0; i<32*32; i++)
     grid.appendChild(newDiv);
 }
 
-function random(n)
-{
-    return Math.floor(Math.random()*n);
-}
 
-function addColor(event)
+function rgbify(event)
 {
-    event.target.style.backgroundColor = 'black';
     
+    if (event.target.value == 'BnW')
+    {
+        boolini = false;
+        event.target.value = 'RGB';
+        event.target.textContent = 'RGB';
+    }
+    else if (event.target.value == 'RGB')
+    {
+        boolini = true;  
+        event.target.value = 'BnW';
+        event.target.textContent = 'BnW';
+    }
 }
 
-function started(event)
-{
-    event.target.style.backgroundColor = 'black';
-    cells.forEach((cell)=>cell.addEventListener('mouseenter',addColor));
-    
-}
 
-function stopped(event)
-{
-    cells.forEach((cell) => cell.removeEventListener('mouseenter',addColor));
-}
+
 
 cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => cell.addEventListener('mousedown',started));
@@ -45,5 +128,17 @@ cells.forEach((cell)=>cell.addEventListener('mouseup',stopped));
 cells.forEach((cell)=> cell.addEventListener('dragstart', (e) => { e.preventDefault() }))
 cells.forEach((cell)=> cell.addEventListener('drop', (e) => { e.preventDefault() }))
 
-// features
+size = document.querySelector('#size');
+size.addEventListener('change',setSize);
+
+clr = document.querySelector('#clr');
+clr.addEventListener('click',clearAll);
+
+rgb = document.querySelector('#rgb');
+rgb.addEventListener('click',rgbify);
+
+
+
+
+
 
