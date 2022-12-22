18abb39d36b1e4bc17cdc0caf364af2bfc6c46da
yash <yashwanthvs@gmail.com>
2022-12-23 01:51:32 +0530

    barebones functionality

--- javascript/photo_tagging/src/App.js
+++ javascript/photo_tagging/src/App.js
@@ -4,11 +4,9 @@ import ImageContainer from "./components/ImageContainer";
 import AppCss from "./styles/app.module.css";
 
 function App() {
-  const [output, setOutput] = useState("");
   return (
-    <div className={AppCss.root}>
-      {output && <div className={AppCss.output}>{output}</div>}
-      <ImageContainer setOutput={setOutput} />
+    <div className={AppCss.rootContainer}>
+      <ImageContainer />
     </div>
   );
 }
--- javascript/photo_tagging/src/components/ImageContainer.js
+++ javascript/photo_tagging/src/components/ImageContainer.js
@@ -1,44 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import mainImage from "../assets/images/rickandmortywaldo.webp";
 
 import AppCss from "../styles/app.module.css";
 import locations from "../data/locations";
+import styles from "../styles/imageContainer.module.css";
 
-const ImageContainer = ({ setOutput }) => {
-  const [boxPositions, setBoxPositions] = useState([]);
+const ImageContainer = () => {
+  const [boxPositions, setBoxPositions] = useState();
+  const [output, setOutput] = useState("");
+  const [character, setCharacter] = useState("");
+  const [score, setScore] = useState(0);
+
+  const targets = ["Beth", "Morty", "Summer"];
 
   const handleClick = (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    setBoxPositions([...boxPositions, { x, y }]);
-
+    setCharacter('')
+    setBoxPositions({ x, y });
     const [X, Y] = [e.pageX, e.pageY];
     console.log("page", X, Y);
-    setOutput("Nothing yet");
     locations.forEach((location) => {
-      const [character, coords] = location;
+      const [name, coords] = location;
       if (coords.x1 < X && X < coords.x2 && coords.y1 < Y && Y < coords.y2) {
-        setOutput(character);
+        setCharacter(name);
       }
     });
   };
 
-  return (
-    <div className={AppCss.ImageContainer}>
-      <img src={mainImage} onClick={handleClick} alt="main waldo" />
-      {boxPositions.map((position, index) => (
+  const handleOptionClick = (event) => {
+    const selection = event.target.textContent;
+    if (selection === character) {
+      setScore(score + 1);
+    } else {
+    }
+  };
+
+  const getBoundingBox = (position) => {
+    if (!position) return;
+    return (
+      <div>
         <div
-          key={index}
+          style={{
+            left: `${position.x - 35}px`,
+            top: `${position.y - 35}px`,
+          }}
+          className={styles.boundingBox}
+        />
+        <div
           style={{
             position: "absolute",
-            left: `${position.x-35}px`,
-            top: `${position.y-35}px`,
-            width: "70px",
-            height: "70px",
-            border: "5px solid red",
+            left: `${position.x + 45}px`,
+            top: `${position.y - 35}px`,
           }}
-        />
-      ))}
+          className={styles.optionsContainer}
+        >
+          {targets.map((target) => (
+            <div
+              key={target}
+              className={styles.optionText}
+              onClick={handleOptionClick}
+            >
+              {target}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    setOutput(score)
+  }, [score])
+
+  return (
+    <div className={styles.imageContainer}>
+      <img src={mainImage} onClick={handleClick} alt="main waldo" />
+      {getBoundingBox(boxPositions)}
+      {output && <div className={styles.output}>{output}</div>}
     </div>
   );
 };
