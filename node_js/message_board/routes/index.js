fcfbd70784293378606f97ac44c301f5a840cc7c
yash <yashwanthvs@gmail.com>
2023-03-25 14:47:59 +0530

    add basic messages view

--- node_js/message_board/routes/index.js
+++ node_js/message_board/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+const messages = [
+  {
+    name: "Yash",
+    message: "sup world",
+    dateAdded: new Date(),
+  },
+  {
+    name: "Sashe",
+    message: "this is sashi",
+    dateAdded: new Date(),
+  },
+  {
+    name: "sharan",
+    message: "hello papalu",
+    dateAdded: new Date(),
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages:messages });
 });
 
 module.exports = router;
