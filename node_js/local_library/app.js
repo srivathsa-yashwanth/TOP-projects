483ec736d9e60d3f9b25259cc1a3070b6364d420
yash <yashwanthvs@gmail.com>
2023-03-29 01:23:49 +0530

    moved mongo connection string to a env file

--- node_js/local_library/app.js
+++ node_js/local_library/app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 var indexRouter = require("./routes/index");
 var catalogRouter = require("./routes/catalog");
@@ -11,8 +12,7 @@ var catalogRouter = require("./routes/catalog");
 var app = express();
 
 mongoose.set("strictQuery", false);
-const mongoDb =
-  "mongodb+srv://yash:918spyder@local-library.drlrlxb.mongodb.net/local-library?retryWrites=true&w=majority";
+const mongoDb = process.env.mongodb_string;
 const connectMongo = async () => {
   await mongoose.connect(mongoDb);
 };
