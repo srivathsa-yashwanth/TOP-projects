84e52081f16276bbe0b3ca542d0dc9787402b4c5
yash <yashwanthvs@gmail.com>
2023-03-31 07:48:50 +0530

    add delete and update pages for some

--- node_js/local_library/controllers/authorController.js
+++ node_js/local_library/controllers/authorController.js
@@ -91,7 +91,7 @@ exports.author_create_post = [
       dateOfDeath: req.body.dateOfDeath,
     });
     if (!errors.isEmpty()) {
-      res.render("author-form", {
+      res.render("author_form", {
         title: "Create Author",
         author,
         errors: errors.array(),
@@ -117,13 +117,66 @@ exports.author_create_post = [
 ];
 
 // Display Author delete form on GET.
-exports.author_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      author_books(callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (!results.author) res.redirect("/catalog/authors");
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.author_books,
+      });
+    }
+  );
 };
 
+
+
 // Handle Author delete on POST.
-exports.author_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.body.authorid }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      console.log(req.body.authorid)
+      if (results.authors_books.length > 0) {
+        // Author has books. Render in same way as for GET route.
+        res.render("author_delete", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.authors_books,
+        });
+        return;
+      }
+      // Author has no books. Delete object and redirect to the list of authors.
+      Author.findByIdAndRemove(req.body.authorid, (err) => {
+        if (err) {
+          return next(err);
+        }
+        // Success - go to author list
+        res.redirect("/catalog/authors");
+      });
+    }
+  );
 };
 
 // Display Author update form on GET.
